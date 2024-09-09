b4b967f70f6fbb1569290e7fa980ee220cfb43dd
Everton Sales <pinheiroe49@gmail.com>
2024-09-09 10:30:58 -0300

    Projeto Final Loja de Games

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+﻿import Carrossel from "../../components/carrossel/Carrossel"
+import ListarProdutos from "../../components/produtos/listarprodutos/ListarProdutos"
+
+function Home() {
+    return (
+        <>
+            <div>
+                <Carrossel />
+            </div>
+            <ListarProdutos />
+        </>
+    )
+}
+
+export default Home
